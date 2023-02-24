ade2a6292af9e600dd3bbf4d538ff5a72e46cfe7
Elena Spytsyna <lspoke.larn@gmail.com>
2023-02-24 18:44:02 +0200

    second screen layout

---
+++ app/src/main/java/com/example/mylibrary/presentation/AddAndEditScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mylibrary.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mylibrary.R
+
+class AddAndEditScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_and_edit_screen)
+    }
+}
