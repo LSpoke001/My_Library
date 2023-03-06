7265eb365c779f749ed63c3a1e1b5ede6cb015bc
Elena Spytsyna <lspoke.larn@gmail.com>
2023-03-06 22:24:01 +0200

    binding book list adapter

--- app/src/main/java/com/example/mylibrary/presentation/BookItemHolder.kt
+++ app/src/main/java/com/example/mylibrary/presentation/BookItemHolder.kt
@@ -2,10 +2,8 @@ package com.example.mylibrary.presentation
 
 import android.view.View
 import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mylibrary.R
 
-class BookItemHolder(val view: View): RecyclerView.ViewHolder(view) {
-    val tvTitle = view.findViewById<TextView>(R.id.tv_title)
-    val tvAuthor = view.findViewById<TextView>(R.id.tv_author)
-}
+class BookItemHolder(val binding: ViewDataBinding): RecyclerView.ViewHolder(binding.root)
